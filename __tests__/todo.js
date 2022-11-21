d9f8f5e5e31f12b9d02b9bde68c7f9de1502e494
nand011-git <nandini7346@gmail.com>
2022-11-21 22:05:30 +0530

    code added

---
+++ __tests__/todo.js
@@ -0,0 +1,61 @@
+const todoList = require('../todo');
+
+const {all, add, markAsComplete, overdue, dueToday, dueLater} = todoList();
+let today = new Date().toLocaleDateString("en-CA");
+
+describe("TodoList test suite", () => {
+    beforeAll(() => {
+        add({ 
+            title: "assignment completion",
+            completed: false,
+            dueDate: new Date().toLocaleDateString("en-CA")
+        });
+    })
+    test("creating new todo", () => {
+        const itemscount = all.length;
+        add(
+            {
+            title: "go walking",
+            completed: false,
+            dueDate: new Date().toLocaleDateString("en-CA")
+            }
+        );
+        expect(all.length).toBe(itemscount + 1);
+    })
+
+    test("mark as complete todo",() => {
+        expect(all[0].completed).toBe(false);
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true);
+    })
+
+    test("due yesterday", () => {
+        let duelist = overdue();
+
+        expect(duelist.every((todo) => {
+            return todo.dueDate < today;
+        })
+        ).toBe(true);
+    })
+
+    test("due today", () => {
+        let duelist = dueToday();
+    
+        expect(duelist.every((todo) => {
+            return todo.dueDate === today;
+          })
+        ).toBe(true);
+        })
+
+        test("due tomorrow", () => {
+            let duelist = dueLater();
+        
+            expect(
+              duelist.every((todo) => {
+                return todo.dueDate > today;
+              })
+            ).toBe(true);
+            })
+
+
+})
---
+++ todo.js
@@ -0,0 +1,45 @@
+const todoList = () => {
+    all = []
+    const add = (todoItem) => {
+      all.push(todoItem)
+    }
+    const markAsComplete = (index) => {
+      all[index].completed = true
+    }
+
+    let today = new Date().toLocaleDateString("en-CA");
+  
+    const overdue = () => {
+        return all.filter((element) => {
+            return element.dueDate < today;
+          });
+      
+    }
+  
+    const dueToday = () => {
+        return all.filter((element) => {
+            return element.dueDate === today;
+          });
+    }
+  
+    const dueLater = () => {
+        return all.filter((element) => {
+            return element.dueDate > today;
+          });
+    }
+  
+    const toDisplayableList = (list) => {
+        return list
+      .map((element) => {
+        _status = element.completed ? "[x]" : "[ ]";
+        date = element.dueDate == today ? "" : element.dueDate;
+
+        return `${_status} ${element.title} ${date}`;
+      })
+      .join("\n");
+      
+}
+    return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+  }
+  
+  module.exports = todoList;
